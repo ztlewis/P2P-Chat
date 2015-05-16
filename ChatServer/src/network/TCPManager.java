0863d55822d4014a0600795d84d4a5f664102c2a
stardust <stardust>
2015-05-16 18:06:25 +1000

    styling for chatserver

--- ChatServer/src/network/TCPManager.java
+++ ChatServer/src/network/TCPManager.java
@@ -14,110 +14,101 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.List;
 
-public class TCPManager implements CommManager{
-	ServerSocket servSocket;
-	Socket clientSocket;
-	
-	public void initSocket() throws IOException
-	{
-		try {
-			servSocket = new ServerSocket(0);
-		} catch (IOException e) {
-			System.out.println("TCP socket could not be initialised.");
-		}
-	}
-	
-	public void initSocket(String address, int port) throws IOException
-	{
-		try {
-			servSocket = new ServerSocket(port);
-		} catch (NumberFormatException e) {
-			System.out.println("Invalid port number.");
-		} catch (IOException e) {
-			System.out.println("TCP socket could not be initialised.");
-		}
-	}
-	
-	public String getServerAddress() throws UnknownHostException
-	{
-		InetAddress tempAddress;
-		tempAddress = servSocket.getInetAddress();
-		return tempAddress.getLocalHost().getHostAddress();
-	}
-	
-	public int getServerPort()
-	{
-		return servSocket.getLocalPort();
-	}
-	
-	public List<String> receivePacket() throws IOException, ClassNotFoundException
-	{
-		List<String> packetData;
-		int length;
-		DataInputStream is;
-		byte[] recvBuffer = new byte[100000];
-		
-		clientSocket = servSocket.accept();
-		is = new DataInputStream(clientSocket.getInputStream());
-		is.read(recvBuffer);
-		
-		ObjectInputStream objIS = new ObjectInputStream(new 
-				ByteArrayInputStream(recvBuffer));
-		packetData = (List<String>) objIS.readObject();
-		//Make sure to add size to the packet info.
-		packetData.add(Integer.toString(sizeOf(packetData).length));
-		return packetData;
-	}
-	
-	public int sendPacket(String rcvAddress, int rcvPort, String header, 
-			List<String> contents) throws IOException
-	{
-		//Add address, port.
-		contents.add(getServerAddress());
-		contents.add(Integer.toString(getServerPort()));
-		
-		DataOutputStream os;
-		byte[] sendBuffer = new byte[100000];
-		clientSocket = new Socket(rcvAddress, rcvPort);
-		
-		os = new DataOutputStream(clientSocket.getOutputStream());
-		sendBuffer = listByteArray(header, contents);
-		os.write(sendBuffer);
-		return sendBuffer.length;
-	}
-	
-	public byte[] listByteArray(String message, List<String> list) {
-		ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
-		ObjectOutputStream    objectOS;
-		
-		list.add(0, message);
+public class TCPManager implements CommManager {
+    ServerSocket servSocket;
+    Socket clientSocket;
 
-		try {
-			objectOS = new ObjectOutputStream(byteArrayOS);
-			objectOS.writeObject(list);
-			objectOS.close();
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
+    public void initSocket() throws IOException {
+        try {
+            servSocket = new ServerSocket(0);
+        } catch (IOException e) {
+            System.out.println("TCP socket could not be initialised.");
+        }
+    }
 
-		return byteArrayOS.toByteArray();
-	}
-	
-	public void clearMsgCache()
-	{
-		
-	}
-	
-	public static byte[] sizeOf(Object obj) throws java.io.IOException
-	{
-		ByteArrayOutputStream byteObject = new ByteArrayOutputStream();
-		ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteObject);
-		objectOutputStream.writeObject(obj);
-		objectOutputStream.flush();
-		objectOutputStream.close();
-		byteObject.close();
-		 
-		return byteObject.toByteArray();
-	}
+    public void initSocket(String address, int port) throws IOException {
+        try {
+            servSocket = new ServerSocket(port);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number.");
+        } catch (IOException e) {
+            System.out.println("TCP socket could not be initialised.");
+        }
+    }
+
+    public String getServerAddress() throws UnknownHostException {
+        InetAddress tempAddress;
+        tempAddress = servSocket.getInetAddress();
+        return tempAddress.getLocalHost().getHostAddress();
+    }
+
+    public int getServerPort() {
+        return servSocket.getLocalPort();
+    }
+
+    public List<String> receivePacket() throws IOException, ClassNotFoundException {
+        List<String> packetData;
+        int length;
+        DataInputStream is;
+        byte[] recvBuffer = new byte[100000];
+
+        clientSocket = servSocket.accept();
+        is = new DataInputStream(clientSocket.getInputStream());
+        is.read(recvBuffer);
+
+        ObjectInputStream objIS = new ObjectInputStream(new
+                ByteArrayInputStream(recvBuffer));
+        packetData = (List<String>) objIS.readObject();
+        //Make sure to add size to the packet info.
+        packetData.add(Integer.toString(sizeOf(packetData).length));
+        return packetData;
+    }
+
+    public int sendPacket(String rcvAddress, int rcvPort, String header,
+                          List<String> contents) throws IOException {
+        //Add address, port.
+        contents.add(getServerAddress());
+        contents.add(Integer.toString(getServerPort()));
+
+        DataOutputStream os;
+        byte[] sendBuffer = new byte[100000];
+        clientSocket = new Socket(rcvAddress, rcvPort);
+
+        os = new DataOutputStream(clientSocket.getOutputStream());
+        sendBuffer = listByteArray(header, contents);
+        os.write(sendBuffer);
+        return sendBuffer.length;
+    }
+
+    public byte[] listByteArray(String message, List<String> list) {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        ObjectOutputStream objectOS;
+
+        list.add(0, message);
+
+        try {
+            objectOS = new ObjectOutputStream(byteArrayOS);
+            objectOS.writeObject(list);
+            objectOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return byteArrayOS.toByteArray();
+    }
+
+    public void clearMsgCache() {
+
+    }
+
+    public static byte[] sizeOf(Object obj) throws java.io.IOException {
+        ByteArrayOutputStream byteObject = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteObject);
+        objectOutputStream.writeObject(obj);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        byteObject.close();
+
+        return byteObject.toByteArray();
+    }
 }
